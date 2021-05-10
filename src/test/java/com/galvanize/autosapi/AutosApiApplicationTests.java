04e1a47e3981b7ef9dfbf2a8f49cbffe8dfb13e6
Paul C <pcutter@cnm.edu>
2021-05-10 10:47:35 -0600

    Initial commit

---
+++ src/test/java/com/galvanize/autosapi/AutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.autosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutosApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
